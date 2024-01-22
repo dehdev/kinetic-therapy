fda6745f3ceee5b4dcde9474bb31dd539f1503c8
Prepelita-Munchiu Alexandru <dehlordeh@gmail.com>
2024-01-22 23:23:29 +0200

    commit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
@@ -26,7 +27,7 @@ export class Home extends Component {
     return (
       <>
         <ThemeProvider theme={responsiveTheme}>
-          <Navbar />
+          <Header />
           <div className="imageContainer">
             <img
               src="assets/images/front-desk.jpg"
@@ -95,8 +96,65 @@ export class Home extends Component {
             </div>
           </div>
           <div className="contentContainer">
-            <div className="aboutUs">test</div>
+            <Typography
+              variant="h2"
+              component="div"
+              sx={{
+                flexGrow: 1,
+                fontFamily: "Varela Round, sans-serif",
+                color: "black",
+              }}
+            >
+              Despre noi{" "}
+            </Typography>
+            <div className="aboutUs">
+              <img
+                src="assets/images/massage.png"
+                alt="massage"
+                style={{
+                  maxWidth: "100%",
+                  maxHeight: "400px",
+                }}
+              />
+              <Typography
+                variant="body1"
+                component="p"
+                sx={{
+                  flexGrow: 1,
+                  fontFamily: "Varela Round, sans-serif",
+                  color: "black",
+                }}
+              >
+                <span className="tab"></span>Echipa Kinetic Therapy vă așteaptă la cabinetul nostru din
+                Brăila, în cartierul Progresul, str. Octav Doicescu, bl. D8,
+                parter, cu cele mai noi terapii din domeniul Recuperării
+                Medicale. Personalul nostru calificat aplică aceste terapii în
+                funcție de recomandarea medicală a pacientului, precum și de
+                indicațiile și contraindicațiile fiecărei terapii în parte:
+                kinetoterapie, reeducare posturală globală, Yumeiho, Terapie
+                Schroth, bandajare neuromusculară, drenaj limfatic manual, masaj
+                terapeutic, reflexoterapie, terapie Vojta, terapie Reiki, etc.
+                <br></br><span className="tab"></span>Ne străduim să ținem în permanență cont de nevoile
+                pacienților noștri și să le oferim cele mai noi terapii
+                existente pe piața medicală. Unele dintre acestea sunt unice pe
+                piața brăileană, cum ar fi terapia Vojta, terapia Schroth și
+                Reeducarea posturală globală RPG. Organizăm frecvent cursuri
+                prin programele de educație medicală continuă la care personalul
+                nostru participă, tocmai pentru a putea oferi pacienților noștri
+                servicii medicale de cea mai înaltă calitate. <br></br><span className="tab"></span>Ne
+                preocupăm și de achiziționarea aparaturii medicale performante
+                pentru orașul nostru, precum terapia TECAR prin achiziționarea
+                unui aparat TECAR WINBACK, elongații vertebrale prin
+                achiziționarea unei mese de elongație BTL, și pentru partea de
+                fizioterapie, o combine BTL care dispune de terapie Laser,
+                Magnetoterapie, Ultrasunet și electroterapie.<br></br><span className="tab"></span>Încercăm
+                să oferim o gamă cât mai largă de terapii, astfel încât să
+                rezolvăm problemele pacienților noștri cât mai eficient! Vă
+                așteptăm cu drag!{" "}
+              </Typography>
+            </div>
           </div>
+          <Footer />
         </ThemeProvider>
       </>
     );
